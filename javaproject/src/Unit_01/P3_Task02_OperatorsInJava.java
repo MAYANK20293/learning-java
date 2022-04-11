f349ee538b0dcc81ec220e9ee4ae9b1688f04dbc
panwa <panwa@LAPTOP-LFF96MVF>
2022-04-11 14:02:20 +0530

    commit

--- javaproject/src/Unit_01/P3_Task02_OperatorsInJava.java
+++ javaproject/src/Unit_01/P3_Task02_OperatorsInJava.java
@@ -1,10 +1,64 @@
 package Unit_01;
 
-public class P3_Task02_OperatorsInJava {
+/*
+ * operators in java is a symbol that is used to perform operations 
+ * sum=a+b;
+ * 	for example: +,-,*,/
+ * 	#types of operators in java
+ * 		-Unary operators : {prefix and postfix}[a++,a--,++a,etc]
+ * 		-Arithematic operators: {*,-,/,+,%}
+ * 		-Shift operators: {<< , >>}
+ * 		-relational operators: {> < <= >= !=
+ * 		-Bitwise operators : {& ^ |}
+ * 		-Logical operators : {&& ||}
+ * 		-ternary operators: {? : }
+ * 		-Assignment operators: {=,+=,-=,/=,%=,&/,>>=,>>=}
+ */
+
+public class P3_task2_operatorsinjava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
+}
+class op
+{
+	void unaryop() {
+		int a=10;
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(a--);
+		System.out.println(--a);
+		
+		
+	}
+	void arithmatic()
+	{
+		int a=10,b=5;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+	void shiftop()
+	{
+		System.out.println(10<<2);
+		System.out.println(10>>2);
+	}
+	void relationalop() {
+		int a=10,b=5;
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+	}
+	void bitwise()
+	{
+		int a=10,b=5,c=20;
+		
+	}
+	
 }

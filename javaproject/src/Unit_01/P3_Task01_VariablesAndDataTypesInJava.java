8fa53cfef774d66ac4b9dae9df8d26de21a47f23
panwa <panwa@LAPTOP-LFF96MVF>
2022-04-11 13:12:30 +0530

    commit

---
+++ javaproject/src/Unit_01/P3_Task01_VariablesAndDataTypesInJava.java
@@ -0,0 +1,10 @@
+epackage Unit_01;
+
+public class P3_Task01_VariablesAndDataTypesInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javaproject/src/Unit_01/P6_Task01_ArraysInJava.java
@@ -0,0 +1,10 @@
+package Unit_01;
+
+public class P6_Task01_ArraysInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
